a0382a44e8a7218983adf66c507b56417ded9d9e
Fabian Rodriguez Cuauhizo <cuauhizo@gmail.com>
2024-02-12 13:43:23 -0600

    Funcionalidad de favoritos

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -22,17 +22,17 @@ const handleSubmit = () => {
             <img class="w-32" src="/img/logo.svg" alt="logotipo" />
           </RouterLink>
         </div>
-        <nav class="flex gap-4">
+        <nav class="flex gap-4 text-white">
           <RouterLink
             :to="{ name: 'inicio' }"
-            class="text-white uppercase font-bold"
+            class="uppercase font-bold"
             active-class="text-orange-500"
           >
             Inicio
           </RouterLink>
           <RouterLink
             :to="{ name: 'favoritos' }"
-            class="text-white uppercase font-bold"
+            class="uppercase font-bold"
             active-class="text-orange-500"
           >
             Favoritos
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -3,9 +3,11 @@
 import { Dialog, DialogPanel, DialogTitle, TransitionChild, TransitionRoot } from '@headlessui/vue'
 import { useModalStore } from '@/stores/modal'
 import { useBebidasStore } from '@/stores/bebidas';
+import { useFavoritosStore } from '@/stores/favoritos';
 
 const modal = useModalStore()
 const bebidas = useBebidasStore()
+const favoritos = useFavoritosStore()
 
 const formatearIngredientes = () => {
   const ingredientesDiv = document.createElement('div')
@@ -64,7 +66,15 @@ const formatearIngredientes = () => {
                   type="button"
                   class="w-full rounded bg-gray-600 p-3 font-bold uppercase text-white shadow hover:bg-gray-500"
                   @click="modal.handleClickModal()"
-                  >Cerrar</button>
+                  >Cerrar
+                </button>
+                <button
+                  type="button"
+                  class="w-full rounded bg-orange-600 p-3 font-bold uppercase text-white shadow hover:bg-orange-500"
+                  @click="favoritos.handleClickFavorito()"
+                  >
+                  {{ modal.textoBoton }}
+                </button>
                 </div>
               </DialogPanel>
             </TransitionChild>
